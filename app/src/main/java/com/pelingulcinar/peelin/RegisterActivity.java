62a924758e7f709395e251ba6c6ca176942f9394
pelingul <=>
2018-05-08 21:07:01 +0300

    Frst commit

---
+++ app/src/main/java/com/pelingulcinar/peelin/RegisterActivity.java
@@ -0,0 +1,151 @@
+package com.pelingulcinar.peelin;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+
+
+    //private Toolbar mToolbar;
+
+    private DatabaseReference mDatabase;
+
+    //progress Dialog
+    //private ProgressDialog mRegProgress;
+
+
+    //Firebase Auth
+
+    private FirebaseAuth mAuth;
+
+    /*public RegisterActivity(ProgressDialog mRegProgress) {
+        this.mRegProgress = mRegProgress;
+    }*/
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Toolbar Set
+        /*mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Create Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+
+
+
+        //Firebase Auth
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //Android Fields
+
+        mDisplayName = (TextInputLayout) findViewById(R.id.reg_display_name);
+        mEmail = (TextInputLayout) findViewById(R.id.reg_email);
+        mPassword = (TextInputLayout) findViewById(R.id.reg_password);
+        Button mCreateBtn = (Button) findViewById(R.id.reg_creat_btn);
+        //Bir üst satırda baştaki Button kelimesi yoktu normalde, sonra hata verince böyle düzeltti
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String display_name = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                if (!TextUtils.isEmpty(display_name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+
+
+                    /*mRegProgress.setTitle("Registering User");
+                    mRegProgress.setMessage("Please wait while we create your account!");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();*/
+
+                    register_user(display_name, email, password);
+
+                }
+
+
+            }
+        });
+    }
+
+
+    private void setSupportActionBar(Toolbar mToolbar) {
+    }
+
+    private void register_user(final String display_name, String email, String password) {
+
+       mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+           @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+
+               if (task.isSuccessful()) {
+
+                   FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                   String uid = current_user.getUid();
+
+                   mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+                   HashMap<String, String> userMap = new HashMap<>();
+                   userMap.put("name", display_name);
+                   userMap.put("status", "Hi There I'm Using Pelin Chat.");
+                   userMap.put("image", "default");
+                   userMap.put("thumb_image","default");
+
+                   mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                       @Override
+                       public void onComplete(@NonNull Task<Void> task) {
+
+                           if(task.isSuccessful()) {
+
+                               //mRegProgress.dismiss();
+
+                  /* Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                   mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                   startActivity(mainIntent);
+                   finish();*/
+
+                           }
+
+                       }
+                   });
+
+
+
+
+               } else {
+
+
+                   //mRegProgress.hide();
+                   Toast.makeText(RegisterActivity.this, "Cannot Sign in. Please check the form and try again.", Toast.LENGTH_LONG).show();
+
+               }
+           }
+       });
+    }
+}
